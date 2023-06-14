178b33644469e803e3bbe79637b87d4532ee8b61
Tim Xia <timxia@cmail.carleton.ca>
2023-06-14 16:29:53 -0400

    Cleaned up file structure and manually tested

--- main/server.js
+++ main/server.js
@@ -1,8 +1,9 @@
 
+//Requires
 const express = require("express");
-var cookieParser = require("cookie-parser");
 const app = express();
-const PORT = 3000;
+
+var cookieParser = require("cookie-parser");
 const bodyParser = require('body-parser'); //parse body of post req
 const bcrypt = require("bcrypt");
 const jwt = require('jsonwebtoken');
@@ -10,92 +11,53 @@ require('dotenv').config();
 
 app.set("view engine", "ejs");
 
+//Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(__dirname + "/views")); 
 
-const dataLayer = require("./../data.js"); //Layered System
+//Data access layer
+const dataLayer = require("./../data.js"); 
 
+//Helper Modules
+const authHelper = require("./authHelper.js")(app);
+
+//Constants (for readability)
 const registerPage = ["/", "/register"];
-
-cookieJwtAuth = (req, res, next) => {
-    const token = req.cookies.token;
-    try {
-        const decoded = jwt.verify(token, process.env.ACCESS_TOKEN);
-        const expirationTime = decoded.exp * 1000; // Expiration time in milliseconds
-
-        if (Date.now() >= expirationTime) {
-            // Token has expired
-            res.clearCookie("token");
-            return res.redirect("/login");
-        }
-
-        req.user = decoded;
-        next();
-    } catch (err) {
-        res.clearCookie("token");
-        return res.redirect("/login");
-    }
-}
-
-checkLogin = (req, res, next) => {
-    const token = req.cookies.token;
-
-    if (token != undefined) { //if the user has logged in
-        return res.redirect("/table");
-    }
-    next();
-}
-
-function authRole(role) {
-    return (req, res, next) => {
-        const dataArr = JSON.parse(`[${dataLayer.readData()}]`); //array with user information
-        const targetUser = dataArr.find(user => user.username === req.user.username);
-
-        if (targetUser.role !== role) {
-            res.status(401)
-            return res.send('Not allowed')
-        }
-
-        next()
-    }
-}
-
-function createUserToken(userName) {
-    const user = {username: userName};
-    const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: "10m" });
-    return token;
-}
+const PORT = 3000;
 
 
 //Routes
 
 //Registry Page:
-app.get(registerPage, checkLogin, (req, res) => {
+app.get(registerPage, authHelper.checkLogin, (req, res) => {
     res.render('register-en');
 });
 
-app.post(registerPage, async (req, res) => { //creates basic accounts 
+app.post(registerPage, async (req, res) => { 
     const dataArr = JSON.parse(`[${dataLayer.readData()}]`); //array with user information
-    const usernameUser = dataArr.find(findUser => findUser.username === req.body.username1);
+
+    const usernameUser = dataArr.find(findUser => findUser.username === req.body.username1); //variables to determine if an account already exists.
     const emailUser = dataArr.find(findUser => findUser.email === req.body.email1);
-    if (usernameUser && emailUser){ //username and email taken
+
+    if (usernameUser && emailUser){     //username and email taken
         res.render('register-en', {msg: '<div class="alert alert-danger"><p>Username and email invalid</p></div>'});
-    } else if (emailUser){ //email taken
+        
+    } else if (emailUser){              //email taken
         res.render('register-en', {msg: '<div class="alert alert-danger"><p>Email already taken</p></div>'});
-    } else if (usernameUser) { //user with that username already exists
+
+    } else if (usernameUser) {          //user with that username already exists
         res.render('register-en', {msg: '<div class="alert alert-danger"><p>Username already taken</p></div>'});
 
     } else {
-        try {
-            const hashedPassword = await bcrypt.hash(req.body.password1, 10); //salt is saved with the hashed password automatically
+        try {   //valid information! Creating account
+            const hashedPassword = await bcrypt.hash(req.body.password1, 10); //hashing salt is saved with the hashed password automatically
             const newUser = JSON.stringify({ email: req.body.email1, username: req.body.username1, password: hashedPassword, role: "basic" });
-            console.log("\nRegistry information (w/hashed): " + newUser);
 
-            dataLayer.addData(newUser);
+            dataLayer.addData(newUser); 
 
             //logging user in:
-            const token = createUserToken(req.body.username1);
+            const token = authHelper.createUserToken(req.body.username1);
 
             res.cookie("token", token, {
                 httpOnly: true,
@@ -109,9 +71,8 @@ app.post(registerPage, async (req, res) => { //creates basic accounts
 });
 
 //Login Page:
-app.get("/login", checkLogin, (req, res) => {
-    if (Boolean(req.query.logout) == true){
-        console.log("user just logged out");
+app.get("/login", authHelper.checkLogin, (req, res) => {
+    if (Boolean(req.query.logout) == true){ //If the user just logged out
         res.render('login-en.ejs', {msg: '<section class="alert alert-success"><p>Logged out successfully</p></section>'});
     } else {
         res.render('login-en.ejs');
@@ -119,25 +80,25 @@ app.get("/login", checkLogin, (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-
     const dataArr = JSON.parse(`[${dataLayer.readData()}]`); //array with user information
     const targetUser = dataArr.find(user => user.username === req.body.username1);
 
-    if (targetUser == null) {
+    if (targetUser == null) { //Invalid username
         return res.status(400).render('login-en', {msg: '<div class="alert alert-danger"><p>Cannot find username</p></div>'});
     }
 
     //if user found:
     try {
-        if (bcrypt.compareSync(req.body.password1, targetUser.password)) { //user is good
-            const token = createUserToken(req.body.username1);
+        if (bcrypt.compareSync(req.body.password1, targetUser.password)) { //User is logged in
+            const token = authHelper.createUserToken(req.body.username1);
 
             res.cookie("token", token, {
                 httpOnly: true,
             });
 
             return res.redirect("/table");
-        } else {
+
+        } else { //Invalid password
             return res.status(400).render('login-en', {msg: '<div class="alert alert-danger"><p>Invalid Password</p></div>'});
         }
     } catch {
@@ -146,24 +107,22 @@ app.post("/login", (req, res) => {
 
 });
 
-
-app.get("/table", cookieJwtAuth, (req, res) => {
+//Data page
+app.get("/table", authHelper.cookieJwtAuth, (req, res) => {
     res.render('table.ejs');
 });
 
-
+//Logout
 app.post('/logout', (req, res) => {
     res.clearCookie("token");
     res.redirect("/login?logout=true");
 });
 
-
+//Start the server
 app.listen(PORT, () => {
     console.log(`Running on port ${PORT}.`);
     console.log("Test this at: ");
     console.log(`http://localhost:${PORT}/register`);
     console.log(`http://localhost:${PORT}/login`);
     console.log(`http://localhost:${PORT}/table`);
-
 });
-
--- main/views/scripts/table.js
+++ main/views/scripts/table.js
@@ -1,4 +1,5 @@
 
+//Load data into table
 $(document).ready(function () {
     $('#dataTable').DataTable({
         ajax: 'data/arrays.txt',
@@ -11,17 +12,14 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('logoutButton').addEventListener('click', logout);
 })
 
-function logout(){ //For testing, prints out the current selected value of the select element.
+function logout(){ 
     fetch(`/logout`, { 
         method: 'POST',
     })
     .then(response => {
-        if (response.ok) {
-            console.log("Logged out successfully");  
+        if (response.ok) { //Logged out successfully 
             window.location.href = response.url;      
-        } else {
-            console.log("Failed logout");
-        }
+        } 
     })
     .catch(error => {
         console.error('Error:', error);
